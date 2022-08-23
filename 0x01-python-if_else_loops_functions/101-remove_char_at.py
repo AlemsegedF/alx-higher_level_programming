bdc9c61ce5d0fb485238870daa6d652fa903ad69
AlemsegedF <alemseged.fekede@gmail.com>
2022-08-23 23:29:17 +0300

    created multiple files

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-        if n < 0:
-            return (str)
-        return (str[:n] + str[n + 1:])
+    temp = ""
+    if (n < 0):
+        return str
+    else:
+        for i in range(len(str)):
+            if (i != n):
+                temp = temp + str[i]
+    return temp
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(1, 10):
-        if i >= j:
-            continue
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}".format(i, j), end=", ")
+for i in range(10):
+    for a in range(1+i+i*10, (10*(i+1))):
+        if a == 89:
+            print('{}'.format(a))
+            break
+        print('{:02d}, '.format(a), end='')

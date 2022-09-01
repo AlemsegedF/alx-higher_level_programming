e1c28b03ed55cc4e7a5559c24f26f7968c38db7a
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-09-01 07:31:06 -0700

    created 10-best_score,py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     max = [0]
     for x in a_dictionary:
         if a_dictionary[x] > max:
             max[0] = a_dictionary[key]
     return max[0]
-

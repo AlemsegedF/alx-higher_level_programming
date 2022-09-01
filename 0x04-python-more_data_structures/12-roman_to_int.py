b62100e84adc39ed2f21b9c4ddd4e76becb714f7
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-09-01 08:55:42 -0700

    created 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    r_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for i in roman_string:
+        if i in r_d:
+            if i is 'I' and roman_string[roman_string.index(i) + 1] in "VX":
+                num = num - 1
+            else:
+                num = num + r_d[i]
+
+    return num
